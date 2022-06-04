4a732c672dcbc9c6c807d2268545b00dd54a1bdb
threddast <giorgiserik@gmail.com>
2022-06-04 16:44:54 +0200

    custom.el

--- custom.el
+++ custom.el
@@ -4,4 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" "f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" "145b5f2222f5d547da141dfec6d0786c9898ccb37a0fa7a77f66f845ef677acf" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default)))
+   '("1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" "f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" "145b5f2222f5d547da141dfec6d0786c9898ccb37a0fa7a77f66f845ef677acf" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-ellipsis ((t (:height 0.8 :inherit 'shadow))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5)))))
